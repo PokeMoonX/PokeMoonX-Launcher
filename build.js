9713adafb72fc9c466e5ed36eb7840d9187b142d
andre <61792917+luuxis@users.noreply.github.com>
2024-04-21 18:43:13 +0200

    add deb rpm tar.gz build

--- build.js
+++ build.js
@@ -107,6 +107,18 @@ class Index {
                     target: [{
                         target: "AppImage",
                         arch: "x64"
+                    },
+                    {
+                        target: "deb",
+                        arch: "x64"
+                    },
+                    {
+                        target: "rpm",
+                        arch: "x64"
+                    },
+                    {
+                        target: "tar.gz",
+                        arch: "x64"
                     }]
                 }
             }
